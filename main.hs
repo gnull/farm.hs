e08ba1f98c3c10e07550b4312466a3a023db86cf
Ivan Oleynikov <io@metrotek.spb.ru>
2017-04-19 18:52:15 +0300

    implement `own` function using readProcess

--- main.hs
+++ main.hs
@@ -4,6 +4,7 @@ import Control.Concurrent (forkIO)
 import Control.Concurrent.Chan (newChan, readChan, writeChan, Chan)
 import qualified Control.Concurrent.ThreadManager as TM
 import Control.Monad
+import System.Process (readProcess)
 
 -- Opponent team, game service, and exploit program types
 type Oppo = String
@@ -11,9 +12,8 @@ type Serv = String
 type Expl = FilePath
 type Flag = String
 
--- This function isn't implemented yet
 own :: Expl -> Oppo -> Serv -> IO [Flag]
-own e o s = pure $ map (++ ("_" ++ s ++ "_" ++ o)) ["flag0", "flag1", "flag2"]
+own e o s = lines <$> readProcess e [o, s] ""
 
 main = do
   let opponents = ["127.0.0.1", "127.0.0.2", "127.0.0.3"]
