cc1bfb7edc6ae23321efb2c7fcecce850cd607a8
Ivan Oleynikov <io@metrotek.spb.ru>
2017-04-19 20:58:01 +0300

    implement NChan and move thread code to separate function

--- main.hs
+++ main.hs
@@ -1,4 +1,5 @@
 import Data.Functor
+import Data.Maybe
 import Control.Applicative
 import Control.Concurrent (forkIO)
 import Control.Concurrent.Chan (newChan, readChan, writeChan, Chan)
@@ -15,6 +16,28 @@ type Flag = String
 own :: Expl -> Oppo -> Serv -> IO [Flag]
 own e o s = lines <$> readProcess e [o, s] ""
 
+thread :: Chan (Maybe Flag) -> Expl -> Oppo -> Serv -> IO ()
+thread c e o s = do
+  fs <- own e o s
+  forM_ fs $ writeNChan c
+  closeNChan c
+
+-- Functions that implement a channel closable by N threads on top of Chan
+
+writeNChan :: Chan (Maybe a) -> a -> IO ()
+writeNChan c = writeChan c . Just
+
+closeNChan :: Chan (Maybe a) -> IO ()
+closeNChan = flip writeChan Nothing
+
+readNChan :: Chan (Maybe a) -> Int -> IO [a]
+readNChan _ 0 = pure []
+readNChan c n = do
+  v <- readChan c
+  case v of
+    Nothing -> readNChan c (n - 1)
+    Just a  -> (a:) <$> readNChan c n
+
 main = do
   let opponents = ["127.0.0.1", "127.0.0.2", "127.0.0.3"]
   let services  = ["1234", "80", "8080"]
@@ -22,10 +45,8 @@ main = do
   let tgs = (,) <$> opponents <*> services
 
   tm <- TM.make
-  c <- newChan
-  forM tgs $ \(o, s) -> TM.fork tm $ do
-      fs <- own exploit o s
-      forM_ fs $ writeChan c
-  sequence $ take (length tgs) $ repeat $
-    readChan c >>= (putStrLn . ("Got flag: " ++))
+  c <- (newChan :: IO (Chan (Maybe String)))
+  forM tgs $ TM.fork tm . (uncurry $ thread c exploit)
+  fs <- readNChan c (length tgs)
+  forM_ fs $ putStrLn . ("Got flag: " ++)
   TM.waitForAll tm
