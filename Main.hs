27fa7885faad50b0a6115cb1f1ab8f09491e5c0f
Ivan Oleynikov <io@metrotek.spb.ru>
2017-04-20 11:55:27 +0300

    REmove services list. Add commandline parsing code.

--- Main.hs
+++ Main.hs
@@ -6,20 +6,18 @@ import Control.Concurrent.Async (forConcurrently)
 import Control.Exception (finally)
 import System.Process (readProcess)
 
+import Options (parse, Options (Options))
+
 -- Opponent team, game service, and exploit program types
 type Oppo = String
-type Serv = String
 type Expl = FilePath
 type Flag = String
 
-own :: Expl -> Oppo -> Serv -> IO [Flag]
-own e o s = lines <$> readProcess e [o, s] ""
+own :: Expl -> Oppo -> IO [Flag]
+own e o = lines <$> readProcess e [o] ""
 
 main = do
-  let opponents = ["127.0.0.1", "127.0.0.2", "127.0.0.3"]
-  let services  = ["1234", "80", "8080"]
-  let exploit = "echo"
-  let tgs = (,) <$> opponents <*> services
-
-  fs <- forConcurrently tgs $ uncurry $ own exploit
+  (Options e oFile) <- parse
+  o <- lines <$> readFile oFile
+  fs <- forConcurrently o $ own e
   forM_ (concat fs) $ putStrLn . ("Got flag: " ++)
