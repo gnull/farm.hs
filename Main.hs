68da81c47e9a40017d70e2486e22622100128041
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2017-04-29 17:33:38 +0300

    Add a template for later development with sqlite3

--- Main.hs
+++ Main.hs
@@ -4,7 +4,9 @@ import Control.Applicative
 import Control.Monad
 import Control.Concurrent (threadDelay)
 import Control.Concurrent.Async (forConcurrently, async, wait, Async, waitAnyCancel, asyncThreadId)
+import Control.Concurrent.MVar (newMVar, takeMVar, putMVar, MVar)
 import Data.List (delete)
+import Database.SQLite (openConnection, closeConnection, SQLiteHandle)
 import System.Process (readProcess)
 import Text.Regex.Posix (getAllTextMatches, (=~))
 
@@ -19,15 +21,17 @@ type Flag = String
 own :: Expl -> [String] -> String -> Oppo -> IO [Flag]
 own e as r o = getAllTextMatches <$> (=~ r) <$> readProcess e (as ++ [o]) ""
 
-thread :: Expl -> [String] -> String -> Oppo -> IO ()
-thread e as r o = forever $ do
+thread :: MVar SQLiteHandle -> Expl -> [String] -> String -> Oppo -> IO ()
+thread db e as r o = forever $ do
   fs <- own e as r o
   forM_ fs $ putStrLn . (("[" ++ o ++ "] got flag: ") ++)
   threadDelay 20000000
 
 main = do
+  db <- openConnection "database.sqlite" >>= newMVar
   srv <- async serverStart
   (Options e as oFile reg) <- parse
   o <- lines <$> readFile oFile
-  as <- forM o $ async . thread e as reg
+  as <- forM o $ async . thread db e as reg
   waitAnyCancel $ srv : as
+  takeMVar db >>= closeConnection
