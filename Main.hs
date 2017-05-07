82816040b1103c9df85bfb7d6d197c8da9d74845
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2017-05-07 17:30:05 +0300

    Incapsulate exploit and its arguments into a tuple

--- Main.hs
+++ Main.hs
@@ -10,22 +10,22 @@ import Text.Regex.Posix (getAllTextMatches, (=~))
 
 import Options (parse, Options (Options))
 
-type Oppo = String      -- Opponent team
-type Expl = FilePath    -- Exploit program
-type Flag = String      -- Flag
-type Flagre = String    -- Flag regex
+type Oppo = String                  -- Opponent team
+type Expl = (FilePath, [String])    -- Exploit program and its extra arguments
+type Flag = String                  -- Flag
+type Flagre = String                -- Flag regex
 
-own :: Expl -> [String] -> Flagre -> Oppo -> IO [Flag]
-own e as r o = getAllTextMatches <$> (=~ r) <$> readProcess e (as ++ [o]) ""
+own :: Expl -> Flagre -> Oppo -> IO [Flag]
+own (e, as) r o = getAllTextMatches <$> (=~ r) <$> readProcess e (as ++ [o]) ""
 
-thread :: Expl -> [String] -> Flagre -> Oppo -> IO ()
-thread e as r o = forever $ do
-  fs <- own e as r o
+thread :: Expl -> Flagre -> Oppo -> IO ()
+thread e r o = forever $ do
+  fs <- own e r o
   forM_ fs $ putStrLn . (("[" ++ o ++ "] got flag: ") ++)
   threadDelay 20000000
 
 main = do
   (Options e as oFile reg) <- parse
   o <- lines <$> readFile oFile
-  as <- forM o $ async . thread e as reg
+  as <- forM o $ async . thread (e, as) reg
   waitAnyCancel as
