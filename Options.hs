98fc10f42f74e047e6d211243d4ab9ca6e186277
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2017-05-30 23:23:11 +0300

    Get rid of unnecessary parenthesis

--- Options.hs
+++ Options.hs
@@ -17,37 +17,37 @@ data Options = Options
 
 sample :: Parser Options
 sample = do
-   exploit <- argument str
-          ( metavar "PROGRAM"
-         <> help "Exploit program" )
-   args <- ( many $ argument str $
+   exploit <- argument str $
+            metavar "PROGRAM"
+         <> help "Exploit program"
+   args <-  many $ argument str $
             metavar "[ARG1 [ARG2 ...]]"
-         <> help "Arguments for exploit PROGRAM")
-   targets <- strOption
-          ( long "targets"
+         <> help "Arguments for exploit PROGRAM"
+   targets <- strOption $
+            long "targets"
          <> short 't'
          <> help "File containing list of target IP addresses"
-         <> metavar "FILE" )
-   sumbit <- strOption
-          ( long "submit"
+         <> metavar "FILE"
+   sumbit <- strOption $
+            long "submit"
          <> short 's'
          <> help "Shell command to submit flags. This command will get flag in `flag` environment variable. E.g. `echo $flag`"
-         <> metavar "COMMAND")
-   jobs <- (read <$> strOption
+         <> metavar "COMMAND"
+   jobs <-  read <$> strOption
           ( long "jobs"
          <> short 'j'
          <> help "Number of parallel running exploit jobs"
-         <> metavar "N"))
-   delay <- (read <$> strOption
+         <> metavar "N")
+   delay <-  read <$> strOption
           ( long "delay"
          <> short 'd'
          <> help "Number of seconds to wait after each exploit run"
-         <> metavar "SECS"))
-   regex <- strOption
-          ( long "flagre"
+         <> metavar "SECS")
+   regex <- strOption $
+            long "flagre"
          <> short 'r'
          <> help "POSIX regex for flag"
-         <> metavar "REGEX")
+         <> metavar "REGEX"
    pure Options {..}
 
 parse :: IO (Options)
