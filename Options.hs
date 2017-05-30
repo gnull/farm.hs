68abd42adc3a28d752cb638805e592f581bf597d
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2017-05-30 23:16:59 +0300

    Rewrite Options using -XRecordWildcards & -XApplicativeDo

--- Options.hs
+++ Options.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE ApplicativeDo              #-}
+
 module Options where
 
 import Options.Applicative
@@ -13,38 +16,39 @@ data Options = Options
   , regex      :: String }
 
 sample :: Parser Options
-sample = Options
-      <$> argument str
+sample = do
+   exploit <- argument str
           ( metavar "PROGRAM"
          <> help "Exploit program" )
-      <*> ( many $ argument str $
+   args <- ( many $ argument str $
             metavar "[ARG1 [ARG2 ...]]"
          <> help "Arguments for exploit PROGRAM")
-      <*> strOption
+   targets <- strOption
           ( long "targets"
          <> short 't'
          <> help "File containing list of target IP addresses"
          <> metavar "FILE" )
-      <*> strOption
+   sumbit <- strOption
           ( long "submit"
          <> short 's'
          <> help "Shell command to submit flags. This command will get flag in `flag` environment variable. E.g. `echo $flag`"
          <> metavar "COMMAND")
-      <*> (read <$> strOption
+   jobs <- (read <$> strOption
           ( long "jobs"
          <> short 'j'
          <> help "Number of parallel running exploit jobs"
          <> metavar "N"))
-      <*> (read <$> strOption
+   delay <- (read <$> strOption
           ( long "delay"
          <> short 'd'
          <> help "Number of seconds to wait after each exploit run"
          <> metavar "SECS"))
-      <*> strOption
+   regex <- strOption
           ( long "flagre"
          <> short 'r'
          <> help "POSIX regex for flag"
          <> metavar "REGEX")
+   pure Options {..}
 
 parse :: IO (Options)
 parse = execParser opts
