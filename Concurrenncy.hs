e7b92225d6a9d20bd7bc97117acab79391e3d8c9
Ivan Oleynikov <io@metrotek.spb.ru>
2017-05-31 19:02:22 +0300

    Add initial version of generic concurrency routines

---
+++ Concurrenncy.hs
@@ -0,0 +1,30 @@
+module Main where
+
+import Control.Applicative
+import Control.Arrow ((&&&), (***))
+import Control.Monad
+import Control.Concurrent (MVar, newMVar, modifyMVar, newChan, Chan, getChanContents, writeChan)
+import Control.Concurrent.Async (async, waitAnyCancel)
+
+popQueue :: MVar [a] -> IO a
+popQueue = flip modifyMVar $ return . (tail &&& head)
+
+workForever :: Int                -- Number of worker jobs to spawn
+            -> [w]                -- Work to be done
+            -> (w -> IO (l, [f])) -- Worker routine that returns log message and its result
+            -> (f -> IO l)        -- Submition routine that saves worker's result
+            -> (l -> IO ())       -- Logger routine that processes logging messages
+            -> IO ()
+workForever jobs todo worker saver logger = do
+  log <- newChan
+  queue <- newMVar $ cycle todo
+  asyncs <- replicateM jobs $ async $ forever $ do
+    w <- popQueue queue
+    (l, fs) <- worker w
+    logger l
+    ls <- mapM saver fs
+    mapM_ logger ls
+    -- (logger *** mapM (logger =<< saver)) =<< worker =<< popQueue queue
+  logJob <- async $ getChanContents log >>= mapM_ putStrLn
+  waitAnyCancel $ logJob : asyncs
+  return ()
