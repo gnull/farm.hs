00915b22de1b14c2f0c9c2582ad8c1f9932a9b88
Ivan Oleynikov <io@metrotek.spb.ru>
2017-06-01 20:35:04 +0300

    Rewrite Main using functions in Concurrency

--- Main.hs
+++ Main.hs
@@ -3,43 +3,32 @@ module Main where
 import Control.Applicative
 import Control.Arrow ((&&&))
 import Control.Monad
-import Control.Concurrent (threadDelay, MVar, modifyMVar, Chan, getChanContents, writeChan)
-import Control.Concurrent.Async (async, waitAnyCancel)
 import System.Process (readProcessWithExitCode, readCreateProcessWithExitCode, CreateProcess (..), proc)
-import System.Exit (ExitCode)
 import Text.Regex.Posix (getAllTextMatches, (=~))
 
 import Options (parse, Options (..))
 import Logging (showLog, LogMsg (..))
+import Concurrenncy (workForever)
 
 type Team = String                  -- Opponent team
 type Flag = String                  -- Flag
 
-popTeam :: MVar [Team] -> IO Team
-popTeam = flip modifyMVar $ return . (tail &&& head)
-
-submitFlag :: Chan LogMsg -> String -> Flag -> IO ()
-submitFlag c s f = do
+submitFlag :: String -> Flag -> IO LogMsg
+submitFlag s f = do
   let p = (proc "sh" ["-xefu"]) { env = Just [("flag", f)] }
   (ret, out, err) <- readCreateProcessWithExitCode p s
-  writeChan c $ SubmMsg s ret out err
+  return $ SubmMsg s ret out err
 
-own :: Options -> Team -> IO [Flag]
+own :: Options -> Team -> IO (LogMsg, [Flag])
 own os team = do
   (ret, out, err) <- readProcessWithExitCode (exploit os) (args os ++ [team]) ""
   let result = getAllTextMatches $ (=~ regex os) $ out
-  writeChan (logChan os) $ ExplMsg ([exploit os] ++ args os ++ [team]) ret out err result
-  return result
-
-thread :: Options -> IO ()
-thread os = forever $ do
-  team <- popTeam (queue os)
-  fs <- own os team
-  forM_ fs $ submitFlag (logChan os) (submit os)
-  threadDelay $ delay os * 1000000
+  return (ExplMsg ([exploit os] ++ args os ++ [team]) ret out err result, result)
 
 main = do
   os <- parse
-  as <- replicateM (jobs os) $ async $ thread os
-  logger <- async $ getChanContents (logChan os) >>= mapM_ (putStrLn . showLog)
-  waitAnyCancel $ logger : as
+  workForever (jobs os)
+              (targets os)
+              (own os)
+              (submitFlag $ submit os)
+              (putStrLn . showLog)
