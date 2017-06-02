c0decbe7014f3f2fa8a1916c2be37a323ffe5c18
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2017-06-02 22:30:20 +0300

    Remove duplicate flags

--- Main.hs
+++ Main.hs
@@ -4,6 +4,7 @@ module Main where
 
 import Control.Applicative
 import Control.Monad
+import Data.List (sort, group)
 import Data.Maybe (maybeToList)
 import System.Process (readProcessWithExitCode, readCreateProcessWithExitCode, CreateProcess (..), proc)
 import Text.Regex.Posix (getAllTextMatches, (=~))
@@ -21,8 +22,11 @@ submitFlag s f = do
 own :: String -> [String] -> String -> String -> IO (LogMsg, [String])
 own exploit args regex team = do
   (ret, out, err) <- readProcessWithExitCode exploit (args ++ [team]) ""
-  let result = getAllTextMatches $ (=~ regex) $ out
+  let result = uniq $ getAllTextMatches $ (=~ regex) $ out
   return $ flip (,) result $ ExplMsg ([exploit] ++ args ++ [team]) ret out err result
+ where
+  uniq :: [String] -> [String]
+  uniq = map head . group . sort
 
 main = do
   Options {..} <- parse
