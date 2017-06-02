a7878eded982ca5b715071553dd1fa6f5fe483d4
Ivan Oleynikov <io@metrotek.spb.ru>
2017-06-02 18:48:04 +0300

    Make terminal color more humane

--- Logging.hs
+++ Logging.hs
@@ -1,5 +1,7 @@
 module Logging (showLog, showLogColor, LogMsg (..)) where
 
+-- Maybe I should use https://hackage.haskell.org/package/ansi-wl-pprint ?
+import System.Console.ANSI
 import System.Exit (ExitCode)
 
 data LogMsg = ExplMsg [String] ExitCode String String [String]
@@ -12,12 +14,17 @@ showLog' cmd (ret, out, err) flags = unlines
   ++ map ("  stdout: " ++) (lines out)
   ++ map ("    flag: " ++) flags
 
+stdoutCode = setSGRCode [SetColor Foreground Vivid Yellow]
+stderrCode = setSGRCode [SetColor Foreground Vivid Magenta]
+flagsCode = setSGRCode [SetColor Foreground Vivid Green, SetBlinkSpeed SlowBlink]
+resetCode = setSGRCode [Reset]
+
 showLogColor' :: Show a => a -> (ExitCode, String, String) -> [String] -> String
 showLogColor' cmd (ret, out, err) flags = unlines
   $  [show cmd ++ " returned " ++ show ret]
-  ++ map ("  stderr: " ++) (lines err)
-  ++ map ("  stdout: " ++) (lines out)
-  ++ map ("    flag: " ++) flags
+  ++ map (("  stderr: " ++) . (stdoutCode ++) . (++ resetCode)) (lines err)
+  ++ map (("  stdout: " ++) . (stderrCode ++) . (++ resetCode)) (lines out)
+  ++ map (("    flag: " ++) . (flagsCode ++) . (++ resetCode)) flags
 
 showLog :: LogMsg -> String
 showLog (ExplMsg cmd ret out err fs) = showLog' cmd (ret, out, err) fs
