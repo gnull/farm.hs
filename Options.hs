7f9c38b85ef2ae3704c113a1a475b895ec4d56cf
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2018-04-14 23:21:07 +0300

    Add usage example to help

--- Options.hs
+++ Options.hs
@@ -57,4 +57,8 @@ parse = execParser opts
     opts = info (sample <**> helper)
       ( fullDesc
      <> progDesc "Run an exploit on list of target hosts"
-     <> header "Helper program for Attack-Defence CTF competitions" )
+     <> header "Helper program for Attack-Defence CTF competitions"
+     <> footer ( "EXAMPLE: "
+              ++ "farm-hs -t <(for i in $(seq 100); do echo \"192.168.34.$i\"; done;) -s 'echo $flag | ncat jury.ctf.com 8080' -j 10 -r 'flag\\{\\w+\\}|galf\\{\\w+\\}' -- ./my-exploit.sh"
+               )
+      )
